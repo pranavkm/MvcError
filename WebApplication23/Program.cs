f952c4c62c90aa50933c849785b714c802773dc7
Pranav K <prkrishn@hotmail.com>
2018-06-28 16:13:35 -0700

    Initial commit

---
+++ WebApplication23/Program.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc.Internal;
+using Microsoft.Extensions.DependencyModel;
+using System.IO;
+using System.Linq;
+
+namespace WebApplication23
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var parts = DefaultAssemblyPartDiscoveryProvider.DiscoverAssemblyParts("WebApplication23");
+            foreach (var item in parts)
+            {
+                System.Console.WriteLine(item.Name);
+            }
+        }
+    }
+}
